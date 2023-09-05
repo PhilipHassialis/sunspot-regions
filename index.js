e7999f0dbe3671081afe1caf9b7ba9fa01cc022b
Philip <philip.alexander.hassialis@gmail.com>
2023-09-05 06:52:46 +0300

    Add error handling

--- index.js
+++ index.js
@@ -1,76 +1,80 @@
-import got from 'got';
-import { JSDOM } from 'jsdom';
+import got from "got";
+import { JSDOM } from "jsdom";
 
 const url = `https://www.spaceweatherlive.com/en/solar-activity/region/8124.html`;
 
-got(url).then(response => {
-
-    const dom = new JSDOM(response.body);
-    const tables = dom.window.document.querySelectorAll('.table-striped');
-
-    // console.log(tables.length)
+try {
+  const res = await got(url, {
+    responseType: "text",
+    resolveBodyOnly: true,
+    retry: {
+      limit: 0,
+    },
+  });
+  if (res.error) {
+    console.log("Caught error in res.error");
+  } else {
+    const dom = new JSDOM(res);
+    const tables = dom.window.document.querySelectorAll(".table-striped");
 
     if (tables.length < 3) {
-        throw new Error('Unexpected page format');
+      throw new Error("Unexpected page format");
     }
-
 
     const tableSunspotRegions = tables[0];
 
     const sunspotRegions = [];
     const top10 = [];
-    const initialLine = "8124,"
+    const initialLine = "8124,";
     let line = "";
 
-    tableSunspotRegions.querySelectorAll('tr').forEach((row, index) => {
-        line = initialLine;
-        const columns = row.querySelectorAll('td');
-        if (columns.length === 6) {
-            columns.forEach((column, index) => {
-                if (index === 0) {
-                    line += column.textContent.trim();
-                } else {
-                    if (column.textContent.trim() === "") {
-                        const iClass = column.querySelector('i').className;
-                        if (iClass.split(' ').length > 1) {
-                            line += `,${iClass.split(' ')[1]}`;
-                        } else {
-                            line += `,`;
-                        }
-                    } else {
-                        line += `,${column.textContent.trim()}`;
-                    }
-                }
-            });
-            sunspotRegions.push(line);
-        }
+    tableSunspotRegions.querySelectorAll("tr").forEach((row, index) => {
+      line = initialLine;
+      const columns = row.querySelectorAll("td");
+      if (columns.length === 6) {
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            line += column.textContent.trim();
+          } else {
+            if (column.textContent.trim() === "") {
+              const iClass = column.querySelector("i").className;
+              if (iClass.split(" ").length > 1) {
+                line += `,${iClass.split(" ")[1]}`;
+              } else {
+                line += `,`;
+              }
+            } else {
+              line += `,${column.textContent.trim()}`;
+            }
+          }
+        });
+        sunspotRegions.push(line);
+      }
     });
 
     if (tables.length === 4) {
-
-        const tableTop10 = tables[1];
-        tableTop10.querySelectorAll('tr').forEach((row, index) => {
-            line=initialLine;
-            const columns = row.querySelectorAll('td');
-            columns.forEach((column, index) => {
-                if (index === 0) {
-                    line += column.textContent.trim();
-                } else {
-                    line += `,${column.textContent.trim()}`;
-                }
-            });
-            if (line !== initialLine) top10.push(line.substring(0, line.length - 1));
+      const tableTop10 = tables[1];
+      tableTop10.querySelectorAll("tr").forEach((row, index) => {
+        line = initialLine;
+        const columns = row.querySelectorAll("td");
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            line += column.textContent.trim();
+          } else {
+            line += `,${column.textContent.trim()}`;
+          }
         });
-
+        if (line !== initialLine)
+          top10.push(line.substring(0, line.length - 1));
+      });
     }
 
-
-
-
-    console.log(sunspotRegions)
+    console.log(sunspotRegions);
 
     if (top10.length) {
-        console.log(top10)
+      console.log(top10);
     }
-});
-
+  }
+} catch (error) {
+  console.log("Caught error in catch");
+}
